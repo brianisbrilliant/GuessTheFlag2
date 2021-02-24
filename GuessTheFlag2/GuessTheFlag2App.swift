6dba57726c274242d28ba8723127493e97437eac
Brian Foster <bfoster6@butlercc.edu>
2021-02-24 09:17:35 -0600

    Initial Commit

---
+++ GuessTheFlag2/GuessTheFlag2App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag2App.swift
+//  GuessTheFlag2
+//
+//  Created by Professor Foster on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
