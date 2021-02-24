95b7caf4a32581532d7e07445e08d5c01139e0a4
Brian Foster <bfoster6@butlercc.edu>
2021-02-24 10:31:16 -0600

    Learning about Stacks

--- GuessTheFlag2/ContentView.swift
+++ GuessTheFlag2/ContentView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Game Over!")
-            .padding()
+        VStack(spacing: 20) {
+            HStack(spacing: 20){
+                Text("0.0")
+                    .font(.largeTitle)
+                Text("0.1")
+                    .font(.largeTitle)
+                Text("0.2")
+                    .font(.largeTitle)
+            }
+            HStack(spacing: 20){
+                Text("1.0")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                Text("1.1")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                Text("1.2")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+            }
+            HStack(spacing: 20){
+                Text("2.0")
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.blue)
+                    .blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                Text("2.1")
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.blue)
+                Text("2.2")
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.blue)
+            }
+            
+        }
+        
+    
+        
     }
 }
 
