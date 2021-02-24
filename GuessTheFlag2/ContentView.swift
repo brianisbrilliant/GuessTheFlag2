f1fd8ccc43709ddd3fdc6f95b4ed5f8ad31144d4
Brian Foster <bfoster6@butlercc.edu>
2021-02-24 10:43:30 -0600

    missing with Stack and colors and gradients

--- GuessTheFlag2/ContentView.swift
+++ GuessTheFlag2/ContentView.swift
@@ -9,42 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 20) {
-            HStack(spacing: 20){
-                Text("0.0")
-                    .font(.largeTitle)
-                Text("0.1")
-                    .font(.largeTitle)
-                Text("0.2")
-                    .font(.largeTitle)
-            }
-            HStack(spacing: 20){
-                Text("1.0")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                Text("1.1")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                Text("1.2")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-            }
-            HStack(spacing: 20){
-                Text("2.0")
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.blue)
-                    .blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-                Text("2.1")
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.blue)
-                Text("2.2")
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.blue)
-            }
-            
+        ZStack {
+            // ZStack builds back-to-front
+            //Color.green.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            LinearGradient(gradient: Gradient(colors: [Color.red, Color.yellow]), startPoint: .bottomLeading, endPoint: .topTrailing).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Color.blue.frame(width: 200, height: 100)
+                .cornerRadius(20)
+            Text("Hello there.")
+                .foregroundColor(.white)
         }
-        
-    
         
     }
 }
